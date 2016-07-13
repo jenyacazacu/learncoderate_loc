61477fdce2573bfd53c64e6a62a2b907f03780fe
jenyacazacu <evghenii0101@gmail.com>
2016-07-12 22:19:08 -0600

    switched to handlebars instead of jade

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -12,7 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server','views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: __dirname + '/app_server/views/layouts' }))
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
